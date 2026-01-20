90939d0cf1e2f25a4fd7f433baa42cebe2955e0e
lohotshreya <lohotshreya@gmail.com>
2026-01-20 17:38:04 +0530

    Add basic variable declarations and output in C++

---
+++ basics/02.variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+    float cgpa = 8.5;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "CGPA: " << cgpa << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
