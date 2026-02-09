13df0fc303117f5ff2b3a72a5e09e2fd2fc537c0
lohotshreya <lohotshreya@gmail.com>
2026-02-09 18:23:43 +0530

    Add program to find the largest of two numbers

---
+++ basics/06.largest_of_two.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a > b)
+        cout << "Largest number is: " << a;
+    else
+        cout << "Largest number is: " << b;
+
+    return 0;
+}
